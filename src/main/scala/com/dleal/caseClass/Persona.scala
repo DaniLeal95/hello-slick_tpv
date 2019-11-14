13d81978645404171d08573ee733e0fd4eb97861
Dleal <dlealreyes@gmail.com>
2019-11-14 08:46:06 +0100

    Persona CRUD complete
    Persona crud complete, test complete

--- src/main/scala/com/dleal/caseClass/Persona.scala
+++ src/main/scala/com/dleal/caseClass/Persona.scala
@@ -4,8 +4,8 @@ import java.sql.{Blob, Date}
 
 case class Persona( id_persona: Option[Int] = None, nombre: Option[String], primerApellido:Option[String],segundoApellido: Option[String],
                     fecha_Nacimiento: Option[Date], email:Option[String], direccion: Option[String], image: Option[Blob] ) extends Serializable {
-
+/*
   override def toString: String = {
     id_persona.getOrElse(None) + "|" + nombre.getOrElse(None) + "|" + primerApellido.getOrElse(None) + "|" + segundoApellido.getOrElse(None) + "|" + image.getOrElse(None)
-  }
+  }*/
 }
